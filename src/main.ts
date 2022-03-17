f66a6fe9ccda37eb8056ea92f332b6d6e5161d29
roomey_rahman <roomeyrahman@gmail.com>
2022-03-17 20:33:15 +0600

    update main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const options = {
+    origin: [/^(.*)/],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+    credentials: true,
+    allowedHeaders:
+      'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for',
+    exposedHeaders: 'X-VIDEOBOOK-KEY,X-VIDEOBOOK-KEY-EXPIRES',
+  };
+
+  app.use(helmet());
+  app.use(cookieParser());
+  app.enableCors(options);
+  await app.listen(PORT);
+  Logger.log(`Server running on http://localhost:${PORT}`, 'Bootstrap');
 }
 bootstrap();
