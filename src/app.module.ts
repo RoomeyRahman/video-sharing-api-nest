10cbbce30903ff48b98db71514775346062af7c8
roomey_rahman <roomeyrahman@gmail.com>
2022-03-17 21:01:58 +0600

    create user module and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpErrorFilter } from './common/filters/http-error.filter';
 import { LoggingInterceptor } from './common/interceptor/logging.interceptor';
 import { TransformInterceptor } from './common/interceptor/transform.interceptor';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import 'dotenv/config';
+
+const DB_CONNECTION = process.env.DB_CONNECTION;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(DB_CONNECTION),
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    // }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
